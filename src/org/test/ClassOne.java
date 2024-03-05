b8055b1906fba9b20e502ba1004d49990216cba6
viji1651997 <viji.lakshmi165@gmail.com>
2024-03-05 15:46:01 +0530

    Branch One Updated

--- src/org/test/ClassOne.java
+++ src/org/test/ClassOne.java
@@ -8,5 +8,13 @@ public class ClassOne {
 	private void m2() {
 		System.out.println("Method 2");
 	}
+	private void m3() {
+	System.out.println("Method 3");
+
+	}
+	private void m4() {
+		System.out.println("Method 4");
+
+	}
 
 }
